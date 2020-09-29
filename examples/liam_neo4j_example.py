fdb39c955c0e98240f19c99f85b3ca821454018d
Liam Considine <liam@spectral.energy>
2020-09-29 18:04:49 -0400

    With a Neo4J example...

---
+++ examples/liam_neo4j_example.py
@@ -0,0 +1,50 @@
+import pdb
+from py2neo import Graph
+
+"""
+docker run --publish=7474:7474 --publish=7687:7687 --volume=$HOME/neo4j/data:/data --env='NEO4JLABS_PLUGINS=["apoc", "n10s"]' --env=NEO4J_AUTH=none neo4j:latest
+"""
+
+# g = Graph("bolt://localhost:7687", user="neo4j", password="neo")
+g = Graph("bolt://localhost:7687")
+try:
+    g.run('CREATE CONSTRAINT n10s_unique_uri ON (r:Resource) ASSERT r.uri IS UNIQUE')
+except Exception:
+    print("Unique Constrain on r:Resource already exists")
+
+
+graph_config = g.run('call n10s.graphconfig.show()').data()
+handleRDFTypes = [c for c in graph_config if c.get('param') == 'handleRDFTypes']
+
+if len(handleRDFTypes) == 1 and handleRDFTypes[0].get('value') == 'LABELS':
+    pass
+else:
+    g.run('call n10s.graphconfig.init()')
+
+# Is this necessary?
+# load_brick_ontology = 'CALL n10s.onto.import.fetch("https://brickschema.org/schema/1.1/Brick.ttl", "Turtle");'
+# load_brick_results = g.run(load_brick_ontology).data()
+
+# How to preview import...
+# stream_triples = 'CALL n10s.rdf.stream.fetch("https://github.com/neo4j-labs/neosemantics/raw/3.5/docs/rdf/nsmntx.ttl","Turtle");'
+# results = g.run(stream_triples)
+
+# This works. . . 
+# import_brick_example = 'CALL n10s.rdf.import.fetch("https://raw.githubusercontent.com/BrickSchema/brick-server/master/examples/data/bldg.ttl", "Turtle");'
+# results = g.run(import_brick_example).data()
+
+# TODO why doesn't this work? umm duh it's not on the docker image...
+# stream_triples = 'CALL n10s.rdf.stream.fetch("file:///Users/liam/spectral_work_1/Brick/examples/custom_brick_v103_sample_graph.ttl","Turtle");'
+# results = g.run(stream_triples)
+# pdb.set_trace()
+
+# This works... 
+import_custom_example = 'CALL n10s.rdf.import.fetch("https://raw.githubusercontent.com/iamliamc/Brick/personal-experiments/examples/custom_brick_v103_sample_graph.ttl", "Turtle");'
+results = g.run(import_custom_example).data()
+pdb.set_trace()
+
+print(result)
+pdb.set_trace()
+
+if False:
+    g.delete_all()
