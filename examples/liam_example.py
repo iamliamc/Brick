cc936f61d191251c8c266f03df07665df9155bb7
Liam Considine <liam@spectral.energy>
2020-10-02 14:27:04 -0400

    More tests...

--- examples/liam_example.py
+++ examples/liam_example.py
@@ -5,7 +5,7 @@ from rdflib import RDF, RDFS, OWL, Namespace, Graph, Literal
 
 g = Graph()
 
-BLDG = Namespace("https://example.com/customer_1/building_1#")
+BLDG = Namespace("customer_1/building_1/#")
 g.bind("bldg", BLDG)
 
 BRICK = Namespace("https://brickschema.org/schema/1.1/Brick#")
@@ -16,13 +16,22 @@ g.add((BRICK.TimeseriesUUID, RDF.type, OWL.Class))
 # # We can make TimeseriesUUID a subclass of the more generic "Tag" class.
 # # It is easy to change this later.
 g.add((BRICK.TimeseriesUUID, RDFS.subClassOf, BRICK.Tag))
+g.add((BRICK.BuildingUUID, RDFS.subClassOf, BRICK.Tag))
+g.add((BRICK.CustomerUUID, RDFS.subClassOf, BRICK.Tag))
+
 
 # (subject, predicate, object)
 g.add((BLDG.BUILDING, RDF.type, BRICK.Building))
 
 # Ability to add information to the nodes via a custom class...
-g.add((BLDG.BUILDING, BRICK.TimeseriesUUID, Literal("UUID-1")))
-print(f"The value BRICK.TimeseriesUUID on BLDG.BUILDING is {g.value(BLDG.BUILDING,BRICK.TimeseriesUUID)}")
+g.add((BLDG.BUILDING, BRICK.BuildingUUID, Literal("0cdd624f-6a95-4c7e-b289-348621d39020")))
+g.add((BLDG.BUILDING, BRICK.CustomerUUID, Literal("NSI")))
+print(f"The value BRICK.BuildingUUID on BLDG.BUILDING is {g.value(BLDG.BUILDING,BRICK.BuildingUUID)}")
+print(f"The value BRICK.CustomerUUID on BLDG.BUILDING is {g.value(BLDG.BUILDING,BRICK.CustomerUUID)}")
+
+
+g.add((BLDG["BUILDING.OAT"], RDF.type, BRICK.Air_Temperature_Setpoint))
+g.add((BLDG["BUILDING.OAT"], BRICK.TimeseriesUUID, Literal("1dcfa6a5-9a68-49bd-981b-c5131a395010")))
 
 g.add((BLDG.BLOCK_A, RDF.type, BRICK.Space))
 
@@ -31,33 +40,26 @@ g.add((BLDG.DISTRICT_HEATING, BRICK.feeds, BLDG.PRIMARY_HOT_WATER))
 
 g.add((BLDG.PRIMARY_HOT_WATER, BRICK.isFedBy, BLDG.DISTRICT_HEATING))
 g.add((BLDG.PRIMARY_HOT_WATER, RDF.type, BRICK.Water_Distribution))
+g.add((BLDG.PRIMARY_HOT_WATER, BRICK.feeds, BLDG["AHU_A"]))
 g.add((BLDG.PRIMARY_COLD_WATER, RDF.type, BRICK.Water_Distribution))
+g.add((BLDG.PRIMARY_HOT_WATER, BRICK.feeds, BLDG["AHU_A"]))
+
 
 g.add((BLDG.AHU_A, RDF.type, BRICK.Air_Handler_Unit))
+g.add((BLDG.AHU_A, BRICK.isFedBy, BLDG.PRIMARY_HOT_WATER))
 
 g.add((BLDG["AHU_A.FAN_COIL"], RDF.type, BRICK.Fan_Coil_Unit))
 
 g.add((BLDG["AHU_A.CALCULATED_SETPOINT"], RDF.type, BRICK.Air_Temperature_Setpoint))
-
+g.add((BLDG["AHU_A.CALCULATED_SETPOINT"], BRICK.TimeseriesUUID, Literal("9437c8c2-57a2-4527-9c11-3c4af3fbc286")))
 g.add((BLDG["AHU_A.CALCULATED_SETPOINT.X1"], RDF.type, BRICK.Outside_Air_Temperature_Low_Reset_Setpoint))
-
+g.add((BLDG["AHU_A.CALCULATED_SETPOINT.X1"], BRICK.TimeseriesUUID, Literal("265a67ed-9d6f-42bf-881b-e29f30adc258")))
 g.add((BLDG["AHU_A.CALCULATED_SETPOINT.X2"], RDF.type, BRICK.Outside_Air_Temperature_High_Reset_Setpoint))
+g.add((BLDG["AHU_A.CALCULATED_SETPOINT.X2"], BRICK.TimeseriesUUID, Literal("fa4959ee-32c2-4ce7-a514-1d39bf288a71")))
 g.add((BLDG["AHU_A.CALCULATED_SETPOINT.Y1"], RDF.type, BRICK.Supply_Air_Temperature_Low_Reset_Setpoint))
+g.add((BLDG["AHU_A.CALCULATED_SETPOINT.Y1"], BRICK.TimeseriesUUID, Literal("8ecffa81-9fb4-431b-8394-594033456df6")))
 g.add((BLDG["AHU_A.CALCULATED_SETPOINT.Y2"], RDF.type, BRICK.Supply_Air_Temperature_High_Reset_Setpoint))
-
-
-for floor in range(9):
-    g.add((BLDG[f"FLOOR_{floor}"], RDF.type, BRICK.Floor))
-    g.add((BLDG.BUILDING, BRICK.hasPart, BLDG[f"FLOOR_{floor}"]))
-    for sub_valve in range(3):
-        g.add((BLDG[f"VAV_A_{floor}_{sub_valve}"], RDF.type, BRICK.Variable_Air_Volume_Box))
-        g.add((BLDG[f"HVAC_Zone_A_{floor}_{sub_valve}"], RDF.type, BRICK.HVAC_Zone))
-        g.add((BLDG[f"VAV_A_{floor}_{sub_valve}"], BRICK.feeds, BLDG[f"HVAC_Zone_A_{floor}_{sub_valve}"]))
-        g.add((BLDG[f"VAV_A_{floor}_{sub_valve}.DPR"], RDF.type, BRICK.Damper))
-        g.add((BLDG[f"VAV_A_{floor}_{sub_valve}.DPRPOS"], RDF.type, BRICK.Damper_Position_Setpoint))
-        g.add((BLDG[f"VAV_A_{floor}_{sub_valve}.DPR"], BRICK.isControlledBy, BLDG[f"VAV_A_{floor}_{sub_valve}.DPRPOS"]))
-
-g.add((BLDG.AHU_A, BRICK.feeds, BLDG.BLOCK_A))
+g.add((BLDG["AHU_A.CALCULATED_SETPOINT.Y2"], BRICK.TimeseriesUUID, Literal("5112c487-8a9f-44fa-9bf8-e7d390d8f613")))
 
 g.add((BLDG["AHU_A.SUPPLY_TEMPERATURE"], RDF.type, BRICK.Supply_Air_Temperature_Sensor))
 g.add((BLDG["AHU_A.CALCULATED_SETPOINT"], BRICK.isMeasuredBy, BLDG["AHU_A.SUPPLY_TEMPERATURE"]))
@@ -66,6 +68,24 @@ g.add((BLDG.AHU_A_CALCULATED_SETPOINT, BRICK.hasPoint, BLDG.AHU_A_CALCULATED_SET
 g.add((BLDG.AHU_A_CALCULATED_SETPOINT, BRICK.hasPoint, BLDG.AHU_A_CALCULATED_SETPOINT_X2))
 g.add((BLDG.AHU_A_CALCULATED_SETPOINT, BRICK.hasPoint, BLDG.AHU_A_CALCULATED_SETPOINT_Y1))
 g.add((BLDG.AHU_A_CALCULATED_SETPOINT, BRICK.hasPoint, BLDG.AHU_A_CALCULATED_SETPOINT_Y2))
+
+g.add((BLDG.BUILDING, BRICK.hasPart, BLDG.AHU_A))
+g.add((BLDG.AHU_A, BRICK.isPartOf, BLDG.BUILDING))
+
+for floor in range(9):
+    g.add((BLDG[f"FLOOR_{floor}"], RDF.type, BRICK.Floor))
+    g.add((BLDG.BUILDING, BRICK.hasPart, BLDG[f"FLOOR_{floor}"]))
+    for sub_valve in range(3):
+        vav_sub_valve = BLDG[f"VAV_A_{floor}_{sub_valve}"]
+        g.add((BLDG[vav_sub_valve], RDF.type, BRICK.Variable_Air_Volume_Box))
+        g.add((BLDG.AHU_A, BRICK.feeds, vav_sub_valve))
+        g.add((BLDG[f"HVAC_Zone_A_{floor}_{sub_valve}"], RDF.type, BRICK.HVAC_Zone))
+        g.add((BLDG[vav_sub_valve], BRICK.feeds, BLDG[f"HVAC_Zone_A_{floor}_{sub_valve}"]))
+        g.add((BLDG[f"VAV_A_{floor}_{sub_valve}.DPR"], RDF.type, BRICK.Damper))
+        g.add((BLDG[f"VAV_A_{floor}_{sub_valve}.DPRPOS"], RDF.type, BRICK.Damper_Position_Setpoint))
+        g.add((BLDG[f"VAV_A_{floor}_{sub_valve}.DPR"], BRICK.isControlledBy, BLDG[f"VAV_A_{floor}_{sub_valve}.DPRPOS"]))
+
+g.add((BLDG.AHU_A, BRICK.feeds, BLDG.BLOCK_A))
 
 # g.parse("../Brick.ttl", format="ttl")
 
@@ -78,12 +98,12 @@ sensors = g.query(
 
 def display_subject_results(subject, results):
     for row in results:
-        import pdb; pdb.set_trace()
+        # import pdb; pdb.set_trace()
         print(f"Subject {subject} -> Predicate {row.p} -> Object {row.o}")
 
 
 display_subject_results("bldg:DISTRICT_HEATING", g.query("SELECT ?p ?o {bldg:DISTRICT_HEATING ?p ?o}"))
-import pdb; pdb.set_trace()
+# import pdb; pdb.set_trace()
 
 # building = g.query("""SELECT ?building WHERE {?building rdf:type brick:Building}""")
 # boiler = g.query("""SELECT ?boiler WHERE {?boiler rdf:type brick:Boiler}""")
