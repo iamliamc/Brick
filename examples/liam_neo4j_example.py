3845f021f8478f912644953a8da3fee031acbf93
Liam Considine <liam@spectral.energy>
2020-10-01 22:08:37 -0400

    This shows the basics...

--- examples/liam_neo4j_example.py
+++ examples/liam_neo4j_example.py
@@ -13,51 +13,38 @@ try:
 except Exception:
     print("Unique Constrain on r:Resource already exists")
 
+print("Clean up graph...")
+g.run('MATCH (n) DETACH DELETE n')
 
+print("Graph config drop...")
+g.run('call n10s.graphconfig.drop')
+
+
+print("Graph init...")
+g.run("call n10s.graphconfig.init({handleVocabUris: 'IGNORE'})")
 graph_config = g.run('call n10s.graphconfig.show()').data()
 handleRDFTypes = [c for c in graph_config if c.get('param') == 'handleRDFTypes']
-
-# if len(handleRDFTypes) == 1 and handleRDFTypes[0].get('value') == 'LABELS':
-#     pass
-# else:
-#    g.run('call n10s.graphconfig.init()')
-
-g.run('MATCH (n) DETACH DELETE n')
-g.run('call n10s.graphconfig.drop')
-# g.run('call n10s.graphconfig.init({handleVocabUris: "IGNORE", handleMultival: "ARRAY"})')
-g.run("call n10s.graphconfig.init({handleVocabUris: 'SHORTEN_STRICT'})")
-
-# # Is this necessary?
-# load_brick_1_1_ontology = 'CALL n10s.onto.import.fetch("https://brickschema.org/schema/1.1/Brick.ttl", "Turtle", {addResourceLabels: true, typesToLabels: true});'
-# # load_brick_1_0_3_ontology = 'CALL n10s.onto.import.fetch("https://brickschema.org/schema/1.0.3/Brick.ttl", "Turtle", {addResourceLabels: "true", typesToLabels: "true"});'
-
-# load_brick_results = g.run(load_brick_1_1_ontology).data()
-
-# How to preview import...
-# stream_triples = 'CALL n10s.rdf.stream.fetch("https://github.com/neo4j-labs/neosemantics/raw/3.5/docs/rdf/nsmntx.ttl","Turtle");'
-# results = g.run(stream_triples)
-
-# This works. . . 
-# import_brick_example = 'CALL n10s.rdf.import.fetch("https://raw.githubusercontent.com/BrickSchema/brick-server/master/examples/data/bldg.ttl", "Turtle");'
-# results = g.run(import_brick_example).data()
-
-# TODO why doesn't this work? umm duh it's not on the docker image...
-# stream_triples = 'CALL n10s.rdf.stream.fetch("file:///Users/liam/spectral_work_1/Brick/examples/custom_brick_v103_sample_graph.ttl","Turtle");'
-# results = g.run(stream_triples)
-# pdb.set_trace()
+print(graph_config)
 
 
+
+input_turtle = 'https://raw.githubusercontent.com/iamliamc/Brick/personal-experiments/examples/custom_brick_v11_sample_graph.ttl'
+print(f"Importing {input_turtle}")
 # This works... 
-import_custom_example = 'CALL n10s.rdf.import.fetch("https://raw.githubusercontent.com/iamliamc/Brick/personal-experiments/examples/custom_brick_v103_sample_graph.ttl", "Turtle");'
+import_custom_example = f'CALL n10s.rdf.import.fetch("{input_turtle}", "Turtle");'
 results = g.run(import_custom_example).data()
 
-nodes = NodeMatcher(g)
-relationships = RelationshipMatcher(g)
-resources = nodes.match("Resource").all()
-building = nodes.match("Building").first()
-pdb.set_trace()
+building = g.nodes.match('Building', TimeseriesUUID='UUID-1').first()
+buildings_relationships = list(g.relationships.match((building, None), 'hasPart').limit(3))
 
-print(result)
+"""
+    MATCH (n)-[:hasPart]->(part) RETURN n, part
+"""
+# Is equivalent to: 
+res = list(g.relationships.match((None, None), 'hasPart'))
+
+ahus = g.relationships.match((g.nodes.match('Air_Handler_Unit').first(), None), None).all()
+
 pdb.set_trace()
 
 if False:
